eca2cab92f9acdbdc2a30939144f3c3ef626ac5c
Andras Wacha <awacha@gmail.com>
2016-09-16 14:46:34 +0200

    Fixing windows start menu generation problems

--- bld.bat
+++ bld.bat
@@ -1,8 +1,8 @@
 set MENU_DIR="%PREFIX%"\Menu
 mkdir "%MENU_DIR%"
-copy menu-temcirclefind.json "%MENU_DIR%"\
+copy menu_temcirclefind.json %MENU_DIR%\
 if errorlevel 1 exit 1
-copy tem_circlefind.ico "%MENU_DIR%"\
+copy tem_circlefind.ico %MENU_DIR%\
 if errorlevel 1 exit 1
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
