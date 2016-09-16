bcd1e35f26063d043ddb3b574ca4a92bf7933629
Andras Wacha <awacha@gmail.com>
2016-09-16 13:32:29 +0200

    Corrected a serious error in bld.bat
    Missing \ in the target of the copy command resulted in the overwriting of the Menu directory.

--- bld.bat
+++ bld.bat
@@ -1,5 +1,8 @@
-"%PYTHON%" setup.py install
+set MENU_DIR="%PREFIX%"\Menu
+mkdir "%MENU_DIR%"
+copy menu-temcirclefind.json "%MENU_DIR%"\
 if errorlevel 1 exit 1
-copy menu-temcirclefind.json "%PREFIX%"/Menu
-copy tem_circlefind.ico "%PREFIX%"/Menu
+copy tem_circlefind.ico "%MENU_DIR%"\
+if errorlevel 1 exit 1
+"%PYTHON%" setup.py install
 if errorlevel 1 exit 1
