7a2a2e2557f0552c38c8c1581badc92d97b147dc
Andras Wacha <awacha@gmail.com>
2016-09-16 12:19:58 +0200

    Conda package: next try.

--- bld.bat
+++ bld.bat
@@ -1,4 +1,5 @@
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
-copy menu.json "%PREFIX%"/Menu
+copy menu-temcirclefind.json "%PREFIX%"/Menu
+copy tem_circlefind.ico "%PREFIX%"/Menu
 if errorlevel 1 exit 1
